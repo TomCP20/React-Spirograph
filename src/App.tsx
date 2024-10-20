e59f8331b8c29d9d5b9e2b91ceca95056b34118b
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-10-20 15:02:22 +0100

    added the ability to change the number of points

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,76 @@
 import { Canvas } from '@react-three/fiber'
 import './App.css'
-import fragmentShader from './shaders/fragmentShader.glsl??raw'
-import vertexShader from './shaders/vertexShader.glsl??raw'
-import { useEffect, useMemo, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { button, useControls } from 'leva';
 import { ShaderMaterial } from 'three';
+import Spirograph from './Spirograph';
 
 export default function App() {
-  const ref = useRef<ShaderMaterial>(null!);
-  const n = 1000;
-  const [{ a1, a2, r1, r2 }, set] = useControls(() => 
-    ({
-      a1: {
-        value: 1,
-        min: -20,
-        max: 20,
-        step: 1,
-      },
-      a2: {
-        value: 1,
-        min: -20,
-        max: 20,
-        step: 1,
-      },
-      r1: {
-        value: 50,
-        min: 50,
-        max: 200,
-        step: 1,
-      },
-      r2: {
-        value: 50,
-        min: 50,
-        max: 200,
-        step: 1,
-      },
-      randomize: button(() => {
-        set({
-          a1: Math.floor(Math.random()*41)-20,
-          a2: Math.floor(Math.random()*41)-20,
-          r1: Math.floor(Math.random()*151)+50,
-          r2: Math.floor(Math.random()*151)+50,
-        })
+  const shaderRef = useRef<ShaderMaterial>(null!);
+  const [{ n, a1, a2, r1, r2 }, set] = useControls(() =>
+  ({
+    n: {
+      value: 1000,
+      min: 2,
+      max: 10000,
+      step: 1,
+    },
+    a1: {
+      value: 1,
+      min: -20,
+      max: 20,
+      step: 1,
+    },
+    a2: {
+      value: 1,
+      min: -20,
+      max: 20,
+      step: 1,
+    },
+    r1: {
+      value: 50,
+      min: 50,
+      max: 200,
+      step: 1,
+    },
+    r2: {
+      value: 50,
+      min: 50,
+      max: 200,
+      step: 1,
+    },
+    randomize: button(() => {
+      set({
+        a1: Math.floor(Math.random() * 41) - 20,
+        a2: Math.floor(Math.random() * 41) - 20,
+        r1: Math.floor(Math.random() * 151) + 50,
+        r2: Math.floor(Math.random() * 151) + 50,
       })
     })
+  })
   );
   useEffect(() => {
-    if (ref.current) {
-      ref.current.uniforms.a1.value = a1/gcd(a1, a2);
-      ref.current.uniforms.a2.value = a2/gcd(a1, a2);
+    if (shaderRef.current) {
+      shaderRef.current.uniforms.a1.value = a1 / gcd(a1, a2);
+      shaderRef.current.uniforms.a2.value = a2 / gcd(a1, a2);
     }
   }, [a1, a2])
   useEffect(() => {
-    if (ref.current) {
-      const scale = 0.8/(r1+r2);
-      ref.current.uniforms.r1.value = r1*scale;
-      ref.current.uniforms.r2.value = r2*scale;
+    if (shaderRef.current) {
+      const scale = 0.8 / (r1 + r2);
+      shaderRef.current.uniforms.r1.value = r1 * scale;
+      shaderRef.current.uniforms.r2.value = r2 * scale;
     }
   }, [r1, r2])
-  const vertices = new Float32Array(n * 3);
-  const uniforms = useMemo(
-    () => (
-      {
-        n: { value: n },
-        a1: { value: 1 },
-        a2: { value: 1 },
-        r1: { value: 0.4 },
-        r2: { value: 0.4 },
-      }
-    ),
-    []
-  );
+  useEffect(() => {
+    if (shaderRef.current) {
+      shaderRef.current.uniforms.n.value = n;
+    }
+  }, [n])
   return (
     <div className="size-[800px] text-center m-auto">
       <Canvas className="bg-black" gl={{ preserveDrawingBuffer: true }}>
-        <lineLoop>
-          <bufferGeometry>
-            <bufferAttribute attach={"attributes-position"} array={vertices} itemSize={3} count={n} />
-          </bufferGeometry>
-          <shaderMaterial fragmentShader={fragmentShader} vertexShader={vertexShader} uniforms={uniforms} ref={ref} />
-        </lineLoop>
+        <Spirograph n={n} shaderRef={shaderRef} />
       </Canvas>
     </div>
   )
---
+++ src/Spirograph.tsx
@@ -0,0 +1,29 @@
+import { MutableRefObject, useMemo } from "react";
+import { ShaderMaterial } from "three";
+
+import fragmentShader from './shaders/fragmentShader.glsl??raw'
+import vertexShader from './shaders/vertexShader.glsl??raw'
+
+export default function Spirograph({ n, shaderRef }: Readonly<{ n: number, shaderRef: MutableRefObject<ShaderMaterial> }>) {
+    const vertices = new Float32Array(n * 3);
+    const uniforms = useMemo(
+        () => (
+            {
+                n: { value: 1000 },
+                a1: { value: 1 },
+                a2: { value: 1 },
+                r1: { value: 0.4 },
+                r2: { value: 0.4 },
+            }
+        ),
+        []
+    );
+    return (
+        <lineLoop>
+            <bufferGeometry>
+                <bufferAttribute attach={"attributes-position"} array={vertices} itemSize={3} count={n} />
+            </bufferGeometry>
+            <shaderMaterial fragmentShader={fragmentShader} vertexShader={vertexShader} uniforms={uniforms} ref={shaderRef} />
+        </lineLoop>
+    );
+}
